578332ac0009f65e27223067085eba6f0ebcd3ab
James ABIB <james.bidule@gmail.com>
2024-12-06 11:00:13 +0100

    chore: Expand fake post data
    Expanded the list of fake posts in the data source. This includes more detailed descriptions for existing posts and addition of new posts to provide a wider range of data for testing purposes.

--- lib/features/features/posts/data/datasources/fake_post_datasource.dart
+++ lib/features/features/posts/data/datasources/fake_post_datasource.dart
@@ -5,9 +5,18 @@ import 'post_datasource.dart';
 
 class FakePostsDataSource extends PostDataSource {
   final List<Post> _fakePosts = [
-    const Post(id: '1', title: 'Lake Assal', description: 'The saltiest lake in the world.'),
-    const Post(id: '2', title: 'Moucha Island', description: 'A beautiful coral island in Djibouti.'),
-    const Post(id: '3', title: 'Arta Beach', description: 'A popular snorkeling spot with incredible marine life.'),
+    const Post(id: '1', title: 'Lake Assal', description: 'The saltiest lake in the world and the lowest point in Africa.'),
+    const Post(id: '2', title: 'Moucha Island', description: 'A beautiful coral island, perfect for snorkeling and diving.'),
+    const Post(id: '3', title: 'Arta Beach', description: 'A popular spot for swimming alongside whale sharks.'),
+    const Post(id: '4', title: 'Day Forest National Park', description: 'A lush green oasis in Djibouti, home to unique wildlife.'),
+    const Post(id: '5', title: 'Lake Abbe', description: 'A surreal landscape with limestone chimneys and a boiling lake.'),
+    const Post(id: '6', title: 'Djibouti City', description: 'The vibrant capital city, offering markets, colonial architecture, an, sea views.'),
+    const Post(id: '7', title: 'Tadjourah', description: 'One of Djibouti’s oldest towns, known for its whitewashed houses and rich history.'),
+    const Post(id: '8', title: 'Doralé Beach', description: 'A pristine beach near Djibouti City, ideal for relaxation and sea views.'),
+    const Post(id: '9', title: 'Ghoubbet Al-Kharab', description: 'The "Gulf of Demons," known for its mysterious waters and strong currents.'),
+    const Post(id: '10', title: 'Ali Sabieh Desert', description: 'A vast desert offering stunning red and golden sand dunes.'),
+    const Post(id: '11', title: 'Goda Mountains', description: 'A picturesque mountain range with breathtaking views and cool temperatures.'),
+    const Post(id: '12', title: 'The Grand Bara Desert', description: 'A vast expanse of white sand, perfect for adventure and exploration.'),
   ];
 
   @override
